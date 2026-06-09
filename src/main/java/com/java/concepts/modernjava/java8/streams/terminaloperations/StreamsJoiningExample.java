d11f55a408cbbc0625ab594302e4dbf09d1a3fcb
sambhavjain10 <sambhav.jain@live.com>
2026-06-09 21:09:17 +0530

    adding joining() terminal operation to streams.

---
+++ src/main/java/com/java/concepts/modernjava/java8/streams/terminaloperations/StreamsJoiningExample.java
@@ -0,0 +1,41 @@
+package com.java.concepts.modernjava.java8.streams.terminaloperations;
+
+import com.java.data.QADatabase;
+import com.java.data.QAEngineer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
+
+public class StreamsJoiningExample {
+
+    public static String joining_1(List<QAEngineer> engineerList){
+
+            return engineerList.stream()
+                    .map(QAEngineer::name)
+                    .collect(joining());
+    }
+
+    public static String joining_2(List<QAEngineer> engineerList){
+
+        return engineerList.stream()
+                .map(QAEngineer::name)
+                .collect(joining(", "));
+    }
+
+    public static String joining_3(List<QAEngineer> engineerList){
+
+        return engineerList.stream()
+                .map(QAEngineer::name)
+                .collect(joining(", ", "(", ")")); //simply importing static method joining()
+    }
+
+    static void main(String[] args) {
+        System.out.println("Demonstrating 3 different overloaded Collectors.joining() terminal operation:");
+        System.out.println(joining_1(QADatabase.getQAEngineers()));
+        System.out.println(joining_2(QADatabase.getQAEngineers()));
+        System.out.println(joining_3(QADatabase.getQAEngineers()));
+
+    }
+}
