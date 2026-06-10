1067ff95c05738e7b2109c2701ae0568042850c8
sambhavjain10 <sambhav.jain@live.com>
2026-06-11 00:21:21 +0530

    adding partitioningBy() Collector concepts

--- src/main/java/com/java/concepts/modernjava/java8/streams/terminaloperations/StreamsPartitioningByExample.java
+++ src/main/java/com/java/concepts/modernjava/java8/streams/terminaloperations/StreamsPartitioningByExample.java
@@ -1,13 +1,45 @@
 package com.java.concepts.modernjava.java8.streams.terminaloperations;
 
-public class StreamsPartitioningByExample {
-    static void main(String[] args) {
-            /*
-            The partitioningBy() method is a special case of groupingBy() that partitions the input elements into two groups based on a given predicate. It returns a Map<Boolean, List<T>>, where the key is true for elements that satisfy the predicate and false for those that do not.
+import com.java.data.QADatabase;
+import com.java.data.QAEngineer;
 
-            Example usage:
-            Map<Boolean, List<QAEngineer>> partitionedByExperience = qaEngineers.stream()
-                    .collect(Collectors.partitioningBy(qaEngineer -> qaEngineer.experience() > 3));
-            */
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toSet;
+
+public class StreamsPartitioningByExample {
+
+    static Predicate<QAEngineer> predicateExp = qaEngineer -> qaEngineer.experience()>3;
+    static Predicate<QAEngineer> predicateLoc = qaEngineer -> qaEngineer.location().equalsIgnoreCase("Pune");
+
+
+    static Map<Boolean, List<QAEngineer>> partitioningByExperience(List<QAEngineer> qaEngineers) {
+
+        return qaEngineers.stream()
+                .collect(partitioningBy(predicateExp));
+    }
+
+    static Map<Boolean, Set<QAEngineer>> partitioningByLocation(List<QAEngineer> qaEngineers) {
+
+        return qaEngineers.stream()
+                .collect(partitioningBy(predicateLoc, toSet()));
+    }
+    static void main(String[] args) {
+
+        System.out.println(partitioningByExperience(QADatabase.getQAEngineers()));
+        System.out.println(partitioningByLocation(QADatabase.getQAEngineers()));
     }
 }
+   /*
+     The partitioningBy() method is a special case of groupingBy() that partitions the input elements into two groups
+     based on a given predicate.
+     It returns a Map<Boolean, List<T>>, where the key is true for elements that satisfy the predicate and false for those that do not.
+
+     Example usage:
+      Map<Boolean, List<QAEngineer>> partitionedByExperience = qaEngineers.stream()
+                    .collect(Collectors.partitioningBy(qaEngineer -> qaEngineer.experience() > 3));
+    */
